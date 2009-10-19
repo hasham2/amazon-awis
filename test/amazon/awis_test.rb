5b60eb23783ba2e5b854ccaedec262565f48c6e2
Hasham <hasham2@gmail.com>
2009-10-19 14:02:07 +0500

    removing my keys from test

--- test/amazon/awis_test.rb
+++ test/amazon/awis_test.rb
@@ -2,8 +2,8 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class Amazon::AwisTest < Test::Unit::TestCase
 
-  AWS_ACCESS_KEY_ID = '0HG5AG5N67J9PV9E4TR2'
-  AWS_SECRET_KEY = 'Wchgn17avCusYj5wW+iK8+D9SiJevSIaz/4Qt1J3'
+  AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID'
+  AWS_SECRET_KEY = 'AWS_SECRET_KEY'
   
   raise "Please specify set your AWS_ACCESS_KEY_ID" if AWS_ACCESS_KEY_ID.empty?
   raise "Please specify set your AWS_SECRET_KEY" if AWS_SECRET_KEY.empty?
